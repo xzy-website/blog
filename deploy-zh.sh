b0bc56756aef7e286a8571a845a0e20ae0f8be5e
黑客少年 <140293713+zhao2022-Ux@users.noreply.github.com>
2025-04-19 11:50:38 +0800

    Update deploy-zh.sh

--- deploy-zh.sh
+++ deploy-zh.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm -rf _config.butterfly.yml
-rm -rf _config.yml
+rm -rf "_config.butterfly.yml"
+rm -rf "_config.yml"
 
 echo "Deleted old files"
 
-cp "config-zh.yml" _config.yml
+cp "config-zh.yml" "_config.yml"
 cp "config-butterfly-zh.yml" "_config.butterfly.yml"
 cp "gulpfile-zh.js" "gulpfile.js"
 cp "links.yml" "source/_data/link.yml"
